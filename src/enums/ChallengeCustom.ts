a2bc69a38e7469c9aee142af4e7186ae8e4d23fa
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-03 09:31:07 -0400

    fix: lint

--- src/enums/ChallengeCustom.ts
+++ src/enums/ChallengeCustom.ts
@@ -1,7 +1,8 @@
 import { validateCustomEnum } from "isaacscript-common";
 
 export const ChallengeCustom = {
-  SPEEDRUN_TIMER: Isaac.GetChallengeIdByName("Speedrun Timer for Multi-Runs"),
+  SPEEDRUN_TIMER: Isaac.GetChallengeIdByName("Multi-Run Timer"),
 } as const;
 
+// eslint-disable-next-line unicorn/no-top-level-side-effects
 validateCustomEnum("ChallengeCustom", ChallengeCustom);
