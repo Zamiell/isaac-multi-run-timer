675305f91d8a0f44fc77145a34d8e22a9e98e817
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-03 11:57:03 -0400

    feat: draw black screen

--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,96 @@
-import { ModCallback } from "isaac-typescript-definitions";
 import {
-  ModCallbackCustom,
+  Keyboard,
+  ModCallback,
+  SoundEffect,
+} from "isaac-typescript-definitions";
+import {
+  fonts,
+  game,
+  KColorDefault,
+  log,
   onChallenge,
   RENDER_FRAMES_PER_SECOND,
+  sfxManager,
+  VectorZero,
 } from "isaacscript-common";
 import { name } from "../package.json";
 import { ChallengeCustom } from "./enums/ChallengeCustom";
 import { mod } from "./mod";
 
+const HOTKEY = Keyboard.F2;
+const blackSprite = Sprite();
+
 const v = {
   persistent: {
     totalNumRenderFrames: 0,
-    runStartTime: null as int | null,
   },
 };
 
 export function main(): void {
-  registerVanillaCallbacks();
-  registerCustomCallbacks();
-  mod.saveDataManager(name, v);
-  Isaac.DebugString(`${name} initialized.`);
-}
-
-function registerVanillaCallbacks() {
   mod.AddCallback(ModCallback.POST_RENDER, postRender); // 2
-  mod.AddCallback(ModCallback.POST_GAME_END, postGameEnd); // 16
+
+  mod.saveDataManager(name, v);
+  mod.setHotkey(HOTKEY, () => {
+    log("Multi-Run Timer reset hotkey pressed.");
+    sfxManager.Play(SoundEffect.THUMBS_UP);
+    v.persistent.totalNumRenderFrames = 0;
+  });
+
+  log(`${name} initialized.`);
 }
 
-function registerCustomCallbacks() {
-  mod.AddCallbackCustom(
-    ModCallbackCustom.INPUT_ACTION_FILTER,
-    inputActionFilter,
-  );
-  mod.AddCallbackCustom(
-    ModCallbackCustom.POST_GAME_STARTED_REORDERED,
-    postGameStartedReorderedFalse,
-    false,
-  );
-}
-
-// ModCallback.POST_RENDER (16)
+// ModCallback.POST_RENDER (2)
 function postRender() {
   if (onChallenge(ChallengeCustom.SPEEDRUN_TIMER)) {
+    const hud = game.GetHUD();
+    hud.SetVisible(false);
+
+    drawBlackScreen();
     drawTimerInfo();
+
+    const player = Isaac.GetPlayer();
+    player.ControlsEnabled = false;
+  } else {
+    v.persistent.totalNumRenderFrames++;
   }
+}
+
+function drawBlackScreen() {
+  if (!blackSprite.IsLoaded()) {
+    blackSprite.Load("gfx/ui/boss/versusscreen.anm2", true);
+    blackSprite.SetFrame("Scene", 0);
+    blackSprite.Scale = Vector(100, 100);
+  }
+
+  blackSprite.RenderLayer(0, VectorZero);
 }
 
 function drawTimerInfo() {
   const seconds = v.persistent.totalNumRenderFrames / RENDER_FRAMES_PER_SECOND;
   const minutes = seconds / 60;
-  const hours = seconds / 60;
+  const hours = minutes / 60;
   const lines = [
     "Multi-Run Timer",
-    "The timer is frozen when you are inside this custom challenge.",
+    "",
+    "This mod will keep track of the render frames that are spent",
+    "while not in the main menu. However, the timer is frozen when",
+    "you are inside this custom challenge.",
+    "",
     "Time so far:",
-    `${v.persistent.totalNumRenderFrames} render frames = ${seconds} seconds = ${minutes} minutes = ${hours} hours`,
-    "Press F2 to reset the timer.",
+    `${v.persistent.totalNumRenderFrames} render frames`,
+    `= ${seconds} seconds`,
+    `= ${minutes} minutes`,
+    `= ${hours} hours`,
+    "",
+    `Press ${Keyboard[HOTKEY]} to reset the timer.`,
   ];
 
   let y = 30;
   for (const line of lines) {
-    Isaac.RenderText(line, 50, y, 1, 1, 1, 255);
+    const isTitle = line === lines[0];
+    const font = isTitle ? fonts.droid : fonts.droid;
+    const scale = isTitle ? 1.5 : 1;
+    font.DrawStringScaled(line, 50, y, scale, scale, KColorDefault);
     y += 20;
   }
-}
-
-// ModCallback.POST_GAME_END (16)
-function postGameEnd() {
-  if (onChallenge(ChallengeCustom.SPEEDRUN_TIMER)) {
-    return;
-  }
-
-  resetTimer();
-}
-
-// ModCallbackCustom.INPUT_ACTION_FILTER
-function inputActionFilter(): boolean | float | undefined {
-  // TODO
-  return undefined;
-}
-
-// ModCallbackCustom.POST_GAME_STARTED_REORDERED
-function postGameStartedReorderedFalse() {
-  if (onChallenge(ChallengeCustom.SPEEDRUN_TIMER)) {
-    return;
-  }
-
-  resetTimer();
-
-  v.persistent.runStartTime = Isaac.GetTime();
-}
-
-function resetTimer() {
-  if (v.persistent.runStartTime === null) {
-    return;
-  }
-
-  const endTime = Isaac.GetTime();
-  const elapsedTime = endTime - v.persistent.runStartTime;
-  v.persistent.totalNumRenderFrames += elapsedTime;
-  v.persistent.runStartTime = null;
 }
--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,8 @@ import { ISCFeature, upgradeMod } from "isaacscript-common";
 import { name } from "../package.json";
 
 const modVanilla = RegisterMod(name, 1);
-const features = [ISCFeature.SAVE_DATA_MANAGER] as const;
+const features = [
+  ISCFeature.CUSTOM_HOTKEYS,
+  ISCFeature.SAVE_DATA_MANAGER,
+] as const;
 export const mod = upgradeMod(modVanilla, features);
